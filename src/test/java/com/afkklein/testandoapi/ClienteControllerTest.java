827669628720d69458ed3b8e665f2dbdf836fc9b
Augusto Klein <idd_aklein@uolinc.com>
2020-09-23 09:34:47 -0300

    uso de imports estaticos para facilitar a leitura do codigo

--- src/test/java/com/afkklein/testandoapi/ClienteControllerTest.java
+++ src/test/java/com/afkklein/testandoapi/ClienteControllerTest.java
@@ -7,20 +7,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.runner.RunWith;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = ClienteController.class)
@@ -52,33 +53,37 @@ public class ClienteControllerTest {
     @Test
     public void getClientesDeveRetornarTodosOsClientes() throws Exception {
         //cenario
-        Mockito.when(clienteRepository.findAll()).thenReturn(Arrays.asList(joao, aarao));
+        when(clienteRepository
+                .findAll())
+                .thenReturn(Arrays.asList(joao, aarao));
 
         List<String> array = Arrays.asList(joaoJsonParse, aaraoJsonParse);
         String response = array.toString();
 
         //acao
-        mockMvc.perform(MockMvcRequestBuilders.get(requestUri))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.content().json(response))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(get(requestUri))
+                .andDo(print())
+                .andExpect(content().json(response))
+                .andExpect(status().isOk());
 
         //verificacao
-        Mockito.verify(clienteRepository).findAll();
+        verify(clienteRepository).findAll();
     }
 
     @Test
     public void getClienteDeveRetornarClienteBuscado() throws Exception {
         //cenario
-        Mockito.when(clienteRepository.findById(2L)).thenReturn(Optional.of(aarao));
+        when(clienteRepository
+                .findById(2L))
+                .thenReturn(Optional.of(aarao));
 
         //acao
-        mockMvc.perform(MockMvcRequestBuilders.get(requestUri + "/{id}", 2))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.content().json(aaraoJsonParse.toString()))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(get(requestUri + "/{id}", 2))
+                .andDo(print())
+                .andExpect(content().json(aaraoJsonParse.toString()))
+                .andExpect(status().isOk());
 
         //verificacao
-        Mockito.verify(clienteRepository).findById(2L);
+        verify(clienteRepository).findById(2L);
     }
 }
